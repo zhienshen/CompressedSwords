0a9444b088e6bcca53063daedfa897d80dda0be9
zhienshen <huan18971374646@163.com>
2025-05-08 18:52:53 +0800

    Initial commit

---
+++ src/client/java/shen/swords/CompressedSwordsClient.java
@@ -0,0 +1,10 @@
+package shen.swords;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CompressedSwordsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/shen/swords/item/ModItems.java
@@ -0,0 +1,61 @@
+package shen.swords.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+//import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import shen.swords.CompressedSwords;
+
+public class ModItems {
+
+    public static final Item test = registerItems("test_item", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl1 = registerItems("compressed_sword1", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl2 = registerItems("compressed_sword2", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl3 = registerItems("compressed_sword3", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl4 = registerItems("compressed_sword4", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl5 = registerItems("compressed_sword5", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl6 = registerItems("compressed_sword6", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl7 = registerItems("compressed_sword7", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl8 = registerItems("compressed_sword8", new Item(new Item.Settings()));
+
+    public static final Item compressed_sword_lvl9 = registerItems("compressed_sword9", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl1 = registerItems("crazy_compressed_sword1", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl2 = registerItems("crazy_compressed_sword2", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl3 = registerItems("crazy_compressed_sword3", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl4 = registerItems("crazy_compressed_sword4", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl5 = registerItems("crazy_compressed_sword5", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl6 = registerItems("crazy_compressed_sword6", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl7 = registerItems("crazy_compressed_sword7", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl8 = registerItems("crazy_compressed_sword8", new Item(new Item.Settings()));
+
+    public static final Item crazy_compressed_sword_lvl9 = registerItems("crazy_compressed_sword9", new Item(new Item.Settings()));
+
+
+
+    private static Item registerItems(String id, Item item) {
+        // return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(CompressedSwords.MOD_ID, id)), item);
+        return Registry.register(Registries.ITEM, Identifier.of(CompressedSwords.MOD_ID, id), item);
+
+    }
+
+    public static void registerItems() {
+        CompressedSwords.LOGGER.info("Registering Mod Items");
+    }
+}

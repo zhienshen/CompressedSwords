cfbfd1031c59ee89fe0e57c56fc9cd6f0bd0f418
zhienshen <huan18971374646@163.com>
2025-05-08 19:15:46 +0800

    更改

--- src/main/java/shen/swords/item/ModItems.java
+++ src/main/java/shen/swords/item/ModItems.java
@@ -1,6 +1,10 @@
 package shen.swords.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 //import net.minecraft.registry.RegistryKey;
@@ -53,9 +57,34 @@ public class ModItems {
         // return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(CompressedSwords.MOD_ID, id)), item);
         return Registry.register(Registries.ITEM, Identifier.of(CompressedSwords.MOD_ID, id), item);
 
+
+    }
+
+    private static void addItemToGroup(FabricItemGroupEntries group) {
+        group.add(test);
+        group.add(compressed_sword_lvl1);
+        group.add(compressed_sword_lvl2);
+        group.add(compressed_sword_lvl3);
+        group.add(compressed_sword_lvl4);
+        group.add(compressed_sword_lvl5);
+        group.add(compressed_sword_lvl6);
+        group.add(compressed_sword_lvl7);
+        group.add(compressed_sword_lvl8);
+        group.add(compressed_sword_lvl9);
+        group.add(crazy_compressed_sword_lvl1);
+        group.add(crazy_compressed_sword_lvl2);
+        group.add(crazy_compressed_sword_lvl3);
+        group.add(crazy_compressed_sword_lvl4);
+        group.add(crazy_compressed_sword_lvl5);
+        group.add(crazy_compressed_sword_lvl6);
+        group.add(crazy_compressed_sword_lvl7);
+        group.add(crazy_compressed_sword_lvl8);
+        group.add(crazy_compressed_sword_lvl9);
+
     }
 
     public static void registerItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemToGroup);
         CompressedSwords.LOGGER.info("Registering Mod Items");
     }
 }
